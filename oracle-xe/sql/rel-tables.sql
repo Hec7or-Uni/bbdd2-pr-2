f8d40cd1b7125846c6ca0f72a64a5fc331e87d58
hector <hectortoraltrabajos@gmail.com>
2023-03-20 18:57:56 +0100

    data

--- oracle-xe/sql/rel-tables.sql
+++ oracle-xe/sql/rel-tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE CLIENTES (
     DNI             VARCHAR(9)      PRIMARY KEY,
-    nombre          VARCHAR(30)    NOT NULL,
-    apellido        VARCHAR(30)    NOT NULL,
+    nombre          VARCHAR(30)     NOT NULL,
+    apellido        VARCHAR(30)     NOT NULL,
     email           VARCHAR(75),
     telefono        VARCHAR(15)     NOT NULL,
     fechaNacimiento DATE            NOT NULL ,
@@ -9,58 +9,40 @@ CREATE TABLE CLIENTES (
     edad            INTEGER         NOT NULL
 );
 
-create table entidad (
-    id              VARCHAR(36)     PRIMARY KEY,
-    codPais         VARCHAR(2)      NOT NULL,
-    codId           VARCHAR(4)      NOT NULL,
-    UNIQUE (codPais, codId)
-);
-
-create table oficina (
+CREATE TABLE OFICINAS (
     codigo          INTEGER         PRIMARY KEY,
     telefono        VARCHAR(15)     NOT NULL,
     direccion       VARCHAR(255)    NOT NULL,
-    idEntidad       VARCHAR(36)     NOT NULL,
-    FOREIGN KEY (idEntidad)         REFERENCES entidad(id)
 );
 
-create table cuenta (
-    id              VARCHAR(24)     PRIMARY KEY,
-    codPais         VARCHAR(2)      NOT NULL,
-    digitosCtrl     VARCHAR(4)      NOT NULL,
-    codId           VARCHAR(2)      NOT NULL,
-    numCuenta       VARCHAR(16)     NOT NULL,
+CREATE TABLE CUENTAS (
+    IBAN            VARCHAR(24)     PRIMARY KEY,
     fechaCreacion   TIMESTAMP       NOT NULL,
-    saldo           NUMBER   		DEFAULT(0),
-    tipoCuenta      VARCHAR(9)      NOT NULL,
+    saldo           NUMBER          DEFAULT(0),
+    tipoCuenta      ENUM('AHORRO', 'CORRIENTE') NOT NULL,
     interes         DECIMAL(4,3),
     oficina         INTEGER         NULL,
-    idEntidad       VARCHAR(36)     NOT NULL,
-    UNIQUE (codPais, codId, digitosCtrl, numCuenta),
-    CHECK (tipoCuenta IN ('ahorro', 'corriente')),
-    CHECK ((tipoCuenta = 'ahorro' AND oficina IS NULL AND interes IS NOT NULL) OR (tipoCuenta = 'corriente' AND oficina IS NOT NULL AND interes IS NULL)),
-    FOREIGN KEY (oficina)           REFERENCES oficina(codigo),
-    FOREIGN KEY (idEntidad)         REFERENCES entidad(id)
+    CHECK ((tipoCuenta = 'AHORRO' AND oficina IS NULL AND interes IS NOT NULL) OR (tipoCuenta = 'CORRIENTE' AND oficina IS NOT NULL AND interes IS NULL)),
+    FOREIGN KEY (oficina)   REFERENCES oficina(codigo),
 );
 
-create table operacion (
+CREATE TABLE OPERACIONES (
     codigo          VARCHAR(36),
     cantidad        NUMBER			NOT NULL,
     timestamp       TIMESTAMP       NOT NULL,
-    tipoOp          VARCHAR(13)     NOT NULL,
+    tipoOp          ENUM('INGRESO', 'RETIRADA', 'TRANSFERENCIA') NOT NULL,
     descripcion     VARCHAR(255),
     cuentaEmisora   VARCHAR(24)		NOT NULL,
     cuentaReceptora VARCHAR(24)		NOT NULL,
     oficina         INTEGER         NULL,
-    CHECK (tipoOp IN ('ingreso', 'retirada', 'transferencia')),
-    CHECK ((tipoOp = 'ingreso' AND cuentaReceptora IS NULL AND oficina IS NOT NULL) OR (tipoOp = 'retirada' AND cuentaReceptora IS NULL AND oficina IS NOT NULL) OR (tipoOp = 'transferencia' AND cuentaReceptora IS NOT NULL AND oficina IS NULL)),
+    CHECK ((tipoOp = 'INGRESO' AND cuentaReceptora IS NULL AND oficina IS NOT NULL) OR (tipoOp = 'RETIRADA' AND cuentaReceptora IS NULL AND oficina IS NOT NULL) OR (tipoOp = 'TRANSFERENCIA' AND cuentaReceptora IS NOT NULL AND oficina IS NULL)),
     PRIMARY KEY (codigo),
     FOREIGN KEY (cuentaEmisora)     REFERENCES cuenta(id),
     FOREIGN KEY (cuentaReceptora)   REFERENCES cuenta(id),
     FOREIGN KEY (oficina)           REFERENCES oficina(codigo)
 );
 
-create table tiene (
+CREATE TABLE TIENEN (
     DNI         VARCHAR(9)      NOT NULL,
     idCuenta    VARCHAR(24)		NOT NULL,
     PRIMARY KEY (DNI, idCuenta),
--- seed/transacciones.py
+++ seed/transacciones.py
@@ -1,6 +1,8 @@
 import random
 import datetime
+import json
 import time
+import uuid
 
 
 def getDate(start="1/1/1970 1:30 PM", end="3/20/2023 4:50 AM", time_format='%m/%d/%Y %I:%M %p', prop=random.random()):
@@ -12,4 +14,33 @@ def getDate(start="1/1/1970 1:30 PM", end="3/20/2023 4:50 AM", time_format='%m/%
     return datetime.datetime.fromtimestamp(ptime).isoformat()[:19].replace('T', ' ')
 
 
-print(getDate())
+with open("MOCK_DATA_CUENTAS.json", "r") as f:
+    cuentas = json.load(f)
+    cuentas = [cuenta["IBAN"] for cuenta in cuentas]
+    f.close()
+
+lista = []
+data = {}
+for i in range(10000):
+    data["codigo"] = str(uuid.uuid4())
+    data["cantidad"] = "{:.3f}".format(random.uniform(0, 25000))
+    data["timestamp"] = getDate()
+    data["descripcion"] = "null"
+    data["tipoOp"] = random.choice(["INGRESO", "RETIRADA", "TRANSFERENCIA"])
+    data["cuentaEmisora"] = random.choice(cuentas)
+    if data["tipoOp"] == "TRANSFERENCIA":
+        data["oficina"] = "null"
+        data["cuentaReceptora"] = random.choice(cuentas)
+        while data["cuentaEmisora"] == data["cuentaReceptora"]:
+            data["cuentaReceptora"] = random.choice(cuentas)
+
+    else:
+        data["oficina"] = 245
+        data["cuentaReceptora"] = "null"
+
+    lista.append(data)
+
+print(lista)
+with open("MOCK_DATA_OPERACIONES.json", "w") as f:
+    json.dump(lista, f, indent=4)
+    f.close()
